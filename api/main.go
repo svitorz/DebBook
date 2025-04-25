3fb489bb7158537c70791210d590b8fc414c192d
svitorz <vitor.fabio.castro@gmail.com>
2025-04-25 01:28:40 -0300

    feat: arquivos de configuração

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,8 +9,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Rodando a API")
+	config.Carregar()
+	fmt.Println("Escutando na porta:", config.Port)
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
