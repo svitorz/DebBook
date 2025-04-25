e0badd568b085d954acc9fc52d41d631f271dd6b
svitorz <vitor.fabio.castro@gmail.com>
2025-04-25 01:15:16 -0300

    feat: criação das tabelas do banco de dados

--- api/sql/script.sql
+++ api/sql/script.sql
@@ -1,15 +1,14 @@
-create database if not exists devbook;
+CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
+DROP TABLE IF EXISTS USUARIOS;
 
-drop table if exists usuarios;
-
-create table usuarios(
+CREATE TABLE USUARIOS(
   id int auto_increment primary key,
   nome varchar(50) not null, 
   nick varchar(50) not null unique,
   email varchar(50) not null unique,
   senha varchar(50) not null, 
   criadoEm timestamp default current_timestamp()
-) ENGINE=INNODB;
+)ENGINE=INNODB;
