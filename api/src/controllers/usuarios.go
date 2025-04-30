6093f8b255a31a3571bb9e7930f2f1eb085d5e0e
svitorz <vitor.fabio.castro@gmail.com>
2025-04-30 18:59:06 -0300

    feat: adicionar função no controller para inserir no banco de dados

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,9 +1,39 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	request, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var usuario models.Usuario
+	if err = json.Unmarshal(request, &usuario); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Conectar()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repository.NewUsersRepository(db)
+	userId, err := repository.Store(usuario)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", userId)))
 }
 
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
