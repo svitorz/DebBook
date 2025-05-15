81561f460b3556dce10bc3cbed4ebb77d81419e5
svitorz <vitor.fabio.castro@gmail.com>
2025-05-15 00:02:24 -0300

    feat: padronizando respostas e validando dados

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -4,36 +4,45 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repository"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	request, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	var usuario models.Usuario
 	if err = json.Unmarshal(request, &usuario); err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := usuario.Preparar(); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
 	}
 
 	db, err := database.Conectar()
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
 	}
+	defer db.Close()
 
 	repository := repository.NewUsersRepository(db)
-	userId, err := repository.Store(usuario)
+	usuario.ID, err = repository.Store(usuario)
 	if err != nil {
-		log.Fatal(err)
+		responses.Err(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("Id inserido: %d", userId)))
+	responses.JSON(w, http.StatusCreated, usuario)
 }
 
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
