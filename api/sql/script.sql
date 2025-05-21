f6d58249a571177f9f6593e22673000dfc4dbfda
svitorz <vitor.fabio.castro@gmail.com>
2025-05-21 19:43:02 -0300

    feat: tabela de seguidores

--- api/sql/script.sql
+++ api/sql/script.sql
@@ -11,4 +11,19 @@ CREATE TABLE USUARIOS(
   email varchar(50) not null unique,
   senha varchar(50) not null, 
   criadoEm timestamp default current_timestamp()
-)ENGINE=INNODB;
+) ENGINE=INNODB;
+
+DROP TABLE IF EXISTS SEGUIDORES;
+
+CREATE TABLE SEGUIDORES(
+  usuario_id int not null,
+  FOREIGN KEY (usuario_id)
+  REFERENCES USUARIOS(id)
+  ON DELETE CASCADE,
+  seguidor_id int not null,
+  FOREIGN KEY (usuario_id)
+  REFERENCES USUARIOS(id)
+  ON DELETE CASCADE,
+
+  PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE=INNODB;
